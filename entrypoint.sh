338c9e350886e0ae612771bb6bc2d779f096028e
Oleksandr Anosov <alexanderanosov1996@gmail.com>
2021-11-18 14:16:28 +0200

    Add flag to run migration (disabled by default)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 set -e
 
+# Default values of arguments
+SHOULD_RUN_MIGRATIONS=0
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        -m|--run-migrations)
+        SHOULD_RUN_MIGRATIONS=1
+        shift # Remove flag from processing
+        ;;
+        *)
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
 . /venv/bin/activate
 
 # Let the DB start
 python -m app.backend_pre_start
 
-# Run migrations
-alembic upgrade head
-
-# Create initial data in DB
-# python ./app/initial_data.py
+if [ $SHOULD_RUN_MIGRATIONS -eq 1 ]; then
+    sh ./run_migrations.sh
+fi
 
 export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
